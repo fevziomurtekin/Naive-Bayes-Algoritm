51d07d0b59536bd9505ec0003fe11e5e8bd3228a
Fevzi Ömür Tekin <fevzi.tekin@ogr.sakarya.edu.tr>
2016-10-21 12:41:01 +0300

    naive Bayes teoremi ile Hepatit tanımı

---
+++ TıbbiOdevi-1/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace TıbbiOdevi_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.listBox6 = new System.Windows.Forms.ListBox();
+            this.listBox7 = new System.Windows.Forms.ListBox();
+            this.listBox8 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(102, -12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1103, 498);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Verileri Listele";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 87);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 35);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Veri gir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 144);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 49);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Sonucu Göster";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(274, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "\tAilenizde hepatit tanısı konulmuş biri var mı?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(273, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "\tDamardan ilaç aldınız mı?\t";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(273, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Homoseksüel misiniz?";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(273, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(213, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Temiz olmayan eşyaları ortak kullandınız mı?";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(273, 162);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(265, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Kan ve kan ürünleriyle uğraşan sağlık personel misiniz?";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(273, 197);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(112, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Diyaliz hastası mısınız?";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(273, 226);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(119, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Hemofili hastası mısınız?";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(273, 255);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(221, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Hepatit Hastalığından şüpheleniyor musunuz?";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Evet",
+            "Hayır"});
+            this.listBox1.Location = new System.Drawing.Point(683, 23);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 30);
+            this.listBox1.TabIndex = 12;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Items.AddRange(new object[] {
+            "Evet",
+            "Hayır"});
+            this.listBox2.Location = new System.Drawing.Point(683, 209);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 30);
+            this.listBox2.TabIndex = 13;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Items.AddRange(new object[] {
+            "Evet",
+            "Hayır"});
+            this.listBox3.Location = new System.Drawing.Point(683, 180);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(120, 30);
+            this.listBox3.TabIndex = 14;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Items.AddRange(new object[] {
+            "Evet",
+            "Hayır"});
+            this.listBox4.Location = new System.Drawing.Point(683, 119);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(120, 30);
+            this.listBox4.TabIndex = 15;
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.Items.AddRange(new object[] {
+            "Evet",
+            "Hayır"});
+            this.listBox5.Location = new System.Drawing.Point(683, 149);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(120, 30);
+            this.listBox5.TabIndex = 16;
+            // 
+            // listBox6
+            // 
+            this.listBox6.FormattingEnabled = true;
+            this.listBox6.Items.AddRange(new object[] {
+            "Evet",
+            "Hayır"});
+            this.listBox6.Location = new System.Drawing.Point(683, 56);
+            this.listBox6.Name = "listBox6";
+            this.listBox6.Size = new System.Drawing.Size(120, 30);
+            this.listBox6.TabIndex = 17;
+            // 
+            // listBox7
+            // 
+            this.listBox7.FormattingEnabled = true;
+            this.listBox7.Items.AddRange(new object[] {
+            "Evet",
+            "Hayır"});
+            this.listBox7.Location = new System.Drawing.Point(683, 87);
+            this.listBox7.Name = "listBox7";
+            this.listBox7.Size = new System.Drawing.Size(120, 30);
+            this.listBox7.TabIndex = 18;
+            // 
+            // listBox8
+            // 
+            this.listBox8.FormattingEnabled = true;
+            this.listBox8.Items.AddRange(new object[] {
+            "Evet",
+            "Hayır"});
+            this.listBox8.Location = new System.Drawing.Point(683, 238);
+            this.listBox8.Name = "listBox8";
+            this.listBox8.Size = new System.Drawing.Size(120, 30);
+            this.listBox8.TabIndex = 19;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(419, 310);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(248, 51);
+            this.button4.TabIndex = 20;
+            this.button4.Text = "Verileri Kaydet";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1204, 498);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBox8);
+            this.Controls.Add(this.listBox7);
+            this.Controls.Add(this.listBox6);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.ListBox listBox6;
+        private System.Windows.Forms.ListBox listBox7;
+        private System.Windows.Forms.ListBox listBox8;
+    }
+}
+
---
+++ TıbbiOdevi-1/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace TıbbiOdevi_1
+{
+    public partial class Form1 : Form
+    {
+
+        MySqlConnection baglanti;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Visible = false; label2.Visible = false; label3.Visible = false; label4.Visible = false; label5.Visible = false; label6.Visible = false; label7.Visible = false; label8.Visible = false;
+            listBox1.Visible = false; listBox2.Visible = false; listBox3.Visible = false; listBox4.Visible = false; listBox5.Visible = false; listBox6.Visible = false; listBox7.Visible = false; listBox8.Visible = false;
+            button4.Visible = false;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Visible = true;
+            String bag;
+            MySqlConnectionStringBuilder build = new MySqlConnectionStringBuilder();
+
+            build.UserID = "root";
+            build.Password = "";
+            build.Database = "hepatittani";
+            build.Server = "localhost";
+
+            bag = build.ToString();
+            baglanti = new MySqlConnection(bag);
+
+            String sql = "SELECT * FROM hepatit";
+            DataTable dt = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            MySqlCommand command = new MySqlCommand();
+
+            command.CommandText = sql;
+            command.Connection = baglanti;
+            adapter.SelectCommand = command;
+
+            baglanti.Open();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+            label1.Visible = false; label2.Visible = false; label3.Visible = false; label4.Visible = false; label5.Visible = false; label6.Visible = false; label7.Visible = false; label8.Visible = false;
+            listBox1.Visible = false; listBox2.Visible = false; listBox3.Visible = false; listBox4.Visible = false; listBox5.Visible = false; listBox6.Visible = false; listBox7.Visible = false; listBox8.Visible = false;
+            button4.Visible = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+          
+            int evet_sayaci = 0,hayir_sayaci= 0 , hepatit_tanisi_evet_sayaci=0, hepatit_tanisi_hayir_sayaci = 0,damar_evet_sayaci=0,damar_hayir_sayaci=0,homo_evet_sayaci=0,homo_hayir_sayaci=0,temiz_evet_sayaci=0,temiz_hayir_sayaci=0,kan_evet_sayaci=0,kan_hayir_sayaci=0,diyaliz_evet_sayaci=0,diyaliz_hayir_sayaci=0, hemofili_evet_sayaci=0, hemofili_hayir_sayaci=0;
+            int veri_sayisi = dataGridView1.RowCount;
+            int soru_sayisi = dataGridView1.ColumnCount;
+            for(int i = 1; i <= veri_sayisi; i++)
+            {
+                for(int y=1;y<=soru_sayisi; y++)
+                {
+                    if (dataGridView1.Rows[i].Cells[8].Equals("Evet"))
+                    {
+                        evet_sayaci++;
+                    }
+                    else if (dataGridView1.Rows[i].Cells[8].Equals("Hayir"))
+                    {
+                        hayir_sayaci++;
+                    }
+                    int hepatit_olasiligi = (evet_sayaci / (evet_sayaci + hayir_sayaci));
+                    if (dataGridView1.Rows[i].Cells[1].Equals("Evet"))
+                    {
+                        hepatit_tanisi_evet_sayaci++;
+                    }
+                    else if (dataGridView1.Rows[i].Cells[1].Equals("Hayir"))
+                    {
+                        hepatit_tanisi_hayir_sayaci++;
+                    }
+                    int hepatit_tani_evet = hepatit_tanisi_evet_sayaci / evet_sayaci;
+                    int hepatit_tani_hayir = hepatit_tanisi_hayir_sayaci / hayir_sayaci;
+
+                    if (dataGridView1.Rows[i].Cells[2].Equals("Evet"))
+                    {
+                        damar_evet_sayaci++;
+                    }
+                    else if (dataGridView1.Rows[i].Cells[2].Equals("Hayir"))
+                    {
+                        damar_hayir_sayaci++;
+                    }
+                    int damar_evet = damar_evet_sayaci / evet_sayaci;
+                    int damar_hayir = damar_hayir_sayaci / hayir_sayaci;
+
+                    if (dataGridView1.Rows[i].Cells[3].Equals("Evet"))
+                    {
+                        homo_evet_sayaci++;
+                    }
+                    else if (dataGridView1.Rows[i].Cells[3].Equals("Hayir"))
+                    {
+                        homo_hayir_sayaci++;
+                    }
+                    int homo_evet = homo_evet_sayaci / evet_sayaci;
+                    int homo_hayir = homo_hayir_sayaci / hayir_sayaci;
+
+                    if (dataGridView1.Rows[i].Cells[3].Equals("Evet"))
+                    {
+                        temiz_evet_sayaci++;
+                    }
+                    else if (dataGridView1.Rows[i].Cells[3].Equals("Hayir"))
+                    {
+                        temiz_hayir_sayaci++;
+                    }
+                    int temiz_evet = temiz_evet_sayaci / evet_sayaci;
+                    int temiz_hayir = temiz_hayir_sayaci / hayir_sayaci;
+
+                    if (dataGridView1.Rows[i].Cells[4].Equals("Evet"))
+                    {
+                        kan_evet_sayaci++;
+                    }
+                    else if (dataGridView1.Rows[i].Cells[4].Equals("Hayir"))
+                    {
+                        kan_hayir_sayaci++;
+                    }
+                    int kan_evet = kan_evet_sayaci / evet_sayaci;
+                    int kan_hayir = kan_hayir_sayaci / hayir_sayaci;
+
+                    if (dataGridView1.Rows[i].Cells[5].Equals("Evet"))
+                    {
+                        diyaliz_evet_sayaci++;
+                    }
+                    else if (dataGridView1.Rows[i].Cells[5].Equals("Hayir"))
+                    {
+                        diyaliz_hayir_sayaci++;
+                    }
+                    int diyaliz_evet = kan_evet_sayaci / evet_sayaci;
+                    int diyaliz_hayir = kan_hayir_sayaci / hayir_sayaci;
+
+                    if (dataGridView1.Rows[i].Cells[8].Equals("Evet"))
+                    {
+                        hemofili_evet_sayaci++;
+                    }
+                    else if (dataGridView1.Rows[i].Cells[8].Equals("Hayir"))
+                    {
+                        hemofili_hayir_sayaci++;
+                    }
+                    int hemofili_evet = hemofili_evet_sayaci / evet_sayaci;
+                    int hemofili_hayir = hemofili_hayir_sayaci / hayir_sayaci;
+
+                    float evet_olasiligi = ((hepatit_tani_evet * damar_evet * homo_evet * temiz_evet * kan_evet * diyaliz_evet * hemofili_evet*evet_sayaci) / 100);
+                    float hayir_olasiligi = ((hepatit_tani_hayir * damar_hayir * homo_hayir * temiz_hayir * kan_hayir * diyaliz_hayir * hemofili_hayir*hayir_sayaci) / 100);
+
+                    if (evet_olasiligi > hayir_olasiligi) { MessageBox.Show("Hepatit belirtileri gözüküyor."); }
+                    else { MessageBox.Show("Hepatit belirtileri gözükmüyor"); }
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            label1.Visible = true; label2.Visible = true; label3.Visible = true; label4.Visible = true; label5.Visible = true; label6.Visible =true; label7.Visible = true; label8.Visible = true;
+            listBox1.Visible = true; listBox2.Visible = true; listBox3.Visible = true; listBox4.Visible = true; listBox5.Visible = true; listBox6.Visible = true; listBox7.Visible = true; listBox8.Visible = true;
+            button4.Visible = true;
+            dataGridView1.Visible = false;
+         
+
+        }
+
+   
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3306;username=root;password=");
+            string insertQuery = "INSERT INTO hepatittani.hepatit(aile,Damar,Homo,temiz,kan,diyaliz,hemofili,hepatit) VALUES ('" + listBox1.SelectedItem.ToString()+ "','" + listBox2.SelectedItem.ToString() + "','" + listBox3.SelectedItem.ToString() + "','" + listBox4.SelectedItem.ToString() + "','" + listBox5.SelectedItem.ToString() + "','" + listBox6.SelectedItem.ToString() + "','" + listBox7.SelectedItem.ToString() + "','" + listBox8.SelectedItem.ToString() + "')";
+            connection.Open();
+            MySqlCommand cmd = new MySqlCommand(insertQuery, connection);
+            try { 
+            if (cmd.ExecuteNonQuery() == 1)
+            {
+                MessageBox.Show("Veri Eklendi");
+
+            }
+            else
+            {
+                MessageBox.Show("Veri eklenemedi");
+            }
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            connection.Close();
+        }
+    }
+}
